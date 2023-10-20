9c50e001c9c2607c7e362e04c5108acf0e9f60a2
imran <87297004+imranshaikh9930@users.noreply.github.com>
2023-10-20 22:17:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+const audio = document.getElementById('audio');
+
+function playAudio(soundFile) {
+    audio.src = `sounds/${soundFile}`;
+    audio.play();
+}
+
+function stopAudio() {
+    audio.pause();
+    audio.currentTime = 0;
+}
